fdda90be3a6dc185a5e065ba2b5a0b8dd79715c3
Juan Pablo <juanpcanoc@hotmail.com>
2019-08-16 15:47:44 -0500

    logicaLista

--- js/main.js
+++ js/main.js
@@ -7,45 +7,49 @@ function seleccionarInicial() {
 }
 
 
+// function ponerFicha(clickid) {
+
+
+
+//     var a = document.getElementsByClassName("col1");
+
+
+//     for (let i = 5; i >= 0; i--) {
+
+//         let b = document.getElementById(a[i].id);
+
+
+
+//         if (b.src == "") {
+//             if (jugadorActual == 1) {
+//                 b.src = srcimg1;
+//                 jugadorActual = 2;
+//                 break;
+//             }
+
+//             b.src = srcimg2;
+//             jugadorActual = 1;
+//             break;
+//         }
+
+
+//     }
+
+
+// }
+
 function ponerFicha(clickid) {
-
-
-
-    var a = document.getElementsByClassName("col1");
-
-
-    for (let i = 5; i >= 0; i--) {
-
-        let b = document.getElementById(a[i].id);
-
-
-
-        if (b.src == "") {
-            if (jugadorActual == 1) {
-                b.src = srcimg1;
-                jugadorActual = 2;
-                break;
-            }
-
-            b.src = srcimg2;
-            jugadorActual = 1;
-            break;
-        }
-
-
-    }
-
-
-}
-
-function ponerFicha2(clickid) {
 
     var arr = [];
 
 
     arr[0] = [document.getElementsByClassName("col1")];
-
     arr[1] = [document.getElementsByClassName("col2")];
+    arr[2] = [document.getElementsByClassName("col3")];
+    arr[3] = [document.getElementsByClassName("col4")];
+    arr[4] = [document.getElementsByClassName("col5")];
+    arr[5] = [document.getElementsByClassName("col6")];
+    arr[6] = [document.getElementsByClassName("col7")];
 
 
     //  alert(arr[1][0][5].id);  acceso al elemento 5 de la columna 1
@@ -69,6 +73,8 @@ function ponerFicha2(clickid) {
             jugadorActual = 1;
             break;
         }
+
+        jugadorActual == 2;
 
     }
 
